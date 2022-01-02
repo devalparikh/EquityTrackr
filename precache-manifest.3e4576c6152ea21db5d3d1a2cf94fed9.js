766fac52d76e0f8f13b539f7aa0ba07cfac4081b
Deval Parikh <devalpp@gmail.com>
2022-01-01 22:27:43 -0500

    Updates

---
+++ precache-manifest.3e4576c6152ea21db5d3d1a2cf94fed9.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4e7cbed57986d7ee1c5db4b966f089",
+    "url": "/EquityTrackr/index.html"
+  },
+  {
+    "revision": "473f0c98023f88dc8fbc",
+    "url": "/EquityTrackr/static/css/5.af2598c0.chunk.css"
+  },
+  {
+    "revision": "cd0b466234245d97d029",
+    "url": "/EquityTrackr/static/css/main.c7775633.chunk.css"
+  },
+  {
+    "revision": "e4114a909e1cb040271f",
+    "url": "/EquityTrackr/static/js/0.31c4bdb5.chunk.js"
+  },
+  {
+    "revision": "404ae38e44a47c5c4161",
+    "url": "/EquityTrackr/static/js/1.8d1320ef.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/EquityTrackr/static/js/1.8d1320ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7951fa6d11476c95d730",
+    "url": "/EquityTrackr/static/js/10.81dc573f.chunk.js"
+  },
+  {
+    "revision": "d9864ffa15e9221e4b2d",
+    "url": "/EquityTrackr/static/js/11.6507108e.chunk.js"
+  },
+  {
+    "revision": "8f3b5944759e25204477",
+    "url": "/EquityTrackr/static/js/12.eeabbc53.chunk.js"
+  },
+  {
+    "revision": "c31289e7b347b5fa6e77",
+    "url": "/EquityTrackr/static/js/13.6e534641.chunk.js"
+  },
+  {
+    "revision": "88fed9d15ad3af942c66",
+    "url": "/EquityTrackr/static/js/14.3ba3c26d.chunk.js"
+  },
+  {
+    "revision": "3b7827c775de90397337",
+    "url": "/EquityTrackr/static/js/15.bc441626.chunk.js"
+  },
+  {
+    "revision": "d0e7fa6928ee84753867",
+    "url": "/EquityTrackr/static/js/16.a0d77c98.chunk.js"
+  },
+  {
+    "revision": "f5f498c0c5dfab9ba1bc",
+    "url": "/EquityTrackr/static/js/17.cc5a2df0.chunk.js"
+  },
+  {
+    "revision": "515e4880ac7ea9cf5908",
+    "url": "/EquityTrackr/static/js/18.35fc6cbf.chunk.js"
+  },
+  {
+    "revision": "9030dfeb22267daeb7c4",
+    "url": "/EquityTrackr/static/js/19.92028857.chunk.js"
+  },
+  {
+    "revision": "54c11db3480e7e1b35ee",
+    "url": "/EquityTrackr/static/js/2.a2e59383.chunk.js"
+  },
+  {
+    "revision": "473f0c98023f88dc8fbc",
+    "url": "/EquityTrackr/static/js/5.e57f7c18.chunk.js"
+  },
+  {
+    "revision": "5c68464f0a04b92c2f1092658dc5a55c",
+    "url": "/EquityTrackr/static/js/5.e57f7c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcfd90d8bd9f5587d54c",
+    "url": "/EquityTrackr/static/js/6.285b6b67.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/EquityTrackr/static/js/6.285b6b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890d347509b5af8b2904",
+    "url": "/EquityTrackr/static/js/7.d3fcf206.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EquityTrackr/static/js/7.d3fcf206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25eab98ae7771a642db",
+    "url": "/EquityTrackr/static/js/8.459067e3.chunk.js"
+  },
+  {
+    "revision": "44655005b66f93df08b6",
+    "url": "/EquityTrackr/static/js/9.08a24cc2.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/EquityTrackr/static/js/9.08a24cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd0b466234245d97d029",
+    "url": "/EquityTrackr/static/js/main.2835de86.chunk.js"
+  },
+  {
+    "revision": "27d3e9569a52f3af2f5e",
+    "url": "/EquityTrackr/static/js/runtime-main.1567f5a2.js"
+  },
+  {
+    "revision": "b9c73089fd14dcb97bf597665f575ac1",
+    "url": "/EquityTrackr/static/media/bell.b9c73089.svg"
+  },
+  {
+    "revision": "ae6d8356288db42a438a49fc4bd53b4e",
+    "url": "/EquityTrackr/static/media/buttons.ae6d8356.svg"
+  },
+  {
+    "revision": "be9df68bf5080eca68b65f6718296fae",
+    "url": "/EquityTrackr/static/media/cards.be9df68b.svg"
+  },
+  {
+    "revision": "977becb6db37f454bad0a5b18f6e647a",
+    "url": "/EquityTrackr/static/media/cart.977becb6.svg"
+  },
+  {
+    "revision": "057753b09a212e5160fe854595ffe79f",
+    "url": "/EquityTrackr/static/media/charts.057753b0.svg"
+  },
+  {
+    "revision": "f7fdff0357cd6bbf4d41ff8aac44a033",
+    "url": "/EquityTrackr/static/media/chat.f7fdff03.svg"
+  },
+  {
+    "revision": "d34c7b50751314310124af5f5b5fbcf3",
+    "url": "/EquityTrackr/static/media/create-account-office-dark.d34c7b50.jpeg"
+  },
+  {
+    "revision": "41b2c6a1c926a8d4f86152f315c7a0bb",
+    "url": "/EquityTrackr/static/media/create-account-office.41b2c6a1.jpeg"
+  },
+  {
+    "revision": "c34040acd2b1249c7cd75ce72a0a36f6",
+    "url": "/EquityTrackr/static/media/dropdown.c34040ac.svg"
+  },
+  {
+    "revision": "a4cc801985d0368571b7e31dfaf62769",
+    "url": "/EquityTrackr/static/media/edit.a4cc8019.svg"
+  },
+  {
+    "revision": "df8aaef727cafdb462dc6a3ef66021d6",
+    "url": "/EquityTrackr/static/media/forbidden.df8aaef7.svg"
+  },
+  {
+    "revision": "6db046dc83427f6c5152a760ec3c9d98",
+    "url": "/EquityTrackr/static/media/forgot-password-office-dark.6db046dc.jpeg"
+  },
+  {
+    "revision": "ac5c499bcee89dd25a8b8c80ed46fb8e",
+    "url": "/EquityTrackr/static/media/forgot-password-office.ac5c499b.jpeg"
+  },
+  {
+    "revision": "37ff675a985578056d0f380370db2e91",
+    "url": "/EquityTrackr/static/media/forms.37ff675a.svg"
+  },
+  {
+    "revision": "77016819da953ea8a4a60950acb99b99",
+    "url": "/EquityTrackr/static/media/github.77016819.svg"
+  },
+  {
+    "revision": "0d8cfa09614809887b157eba25082af1",
+    "url": "/EquityTrackr/static/media/heart.0d8cfa09.svg"
+  },
+  {
+    "revision": "0717747219593ac12c6f355006f97943",
+    "url": "/EquityTrackr/static/media/home.07177472.svg"
+  },
+  {
+    "revision": "cb4a995ff675060363e7489041041b85",
+    "url": "/EquityTrackr/static/media/login-office-dark.cb4a995f.jpeg"
+  },
+  {
+    "revision": "72742c2e91eb0159f6b65c8eefc13602",
+    "url": "/EquityTrackr/static/media/login-office.72742c2e.jpeg"
+  },
+  {
+    "revision": "e8edbe5b89f1a31eb88a2ff2d3cb34eb",
+    "url": "/EquityTrackr/static/media/mail.e8edbe5b.svg"
+  },
+  {
+    "revision": "df9ba7c81d8c805ebf3d84a48bd9d57c",
+    "url": "/EquityTrackr/static/media/menu.df9ba7c8.svg"
+  },
+  {
+    "revision": "62a5a8fff7ff6bef7bf961f2a889dc21",
+    "url": "/EquityTrackr/static/media/modals.62a5a8ff.svg"
+  },
+  {
+    "revision": "f88a7a4e766a011150d1f19f3828bb51",
+    "url": "/EquityTrackr/static/media/money.f88a7a4e.svg"
+  },
+  {
+    "revision": "cdf19a70cc4aad218e3b50d3ee1f6866",
+    "url": "/EquityTrackr/static/media/moon.cdf19a70.svg"
+  },
+  {
+    "revision": "570b0d66f31d545fdbdc30f5eefaa126",
+    "url": "/EquityTrackr/static/media/outlineCog.570b0d66.svg"
+  },
+  {
+    "revision": "42f24449f7bd9928f5ea1d0fd07d95e7",
+    "url": "/EquityTrackr/static/media/outlineLogout.42f24449.svg"
+  },
+  {
+    "revision": "50f0ae3e598fe90d94336b5492c772c1",
+    "url": "/EquityTrackr/static/media/outlinePerson.50f0ae3e.svg"
+  },
+  {
+    "revision": "6650d5109fafc0724eac274c3b43eed5",
+    "url": "/EquityTrackr/static/media/pages.6650d510.svg"
+  },
+  {
+    "revision": "c4fada77c844618fef90d2194039d452",
+    "url": "/EquityTrackr/static/media/people.c4fada77.svg"
+  },
+  {
+    "revision": "b53d01858365021ea008dbad9259a963",
+    "url": "/EquityTrackr/static/media/profile.b53d0185.jpeg"
+  },
+  {
+    "revision": "02b8ece64085c23ec4e599238c9214cb",
+    "url": "/EquityTrackr/static/media/sampleCode.02b8ece6.txt"
+  },
+  {
+    "revision": "70205477154d7db80b2f05e9716eb3c1",
+    "url": "/EquityTrackr/static/media/search.70205477.svg"
+  },
+  {
+    "revision": "e72f14b8a27c721f10d5eb87e8ec3d2f",
+    "url": "/EquityTrackr/static/media/sun.e72f14b8.svg"
+  },
+  {
+    "revision": "b9ce44e708d154ec60a8deabeb3e8e58",
+    "url": "/EquityTrackr/static/media/tables.b9ce44e7.svg"
+  },
+  {
+    "revision": "f3c7c3b232acfe1a6250f640a0a17741",
+    "url": "/EquityTrackr/static/media/trash.f3c7c3b2.svg"
+  },
+  {
+    "revision": "c6bd77d7d4321627a185c1cbc89276d8",
+    "url": "/EquityTrackr/static/media/twitter.c6bd77d7.svg"
+  }
+]);
---
+++ static/js/18.35fc6cbf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwindmill-dashboard-react"]=this["webpackJsonpwindmill-dashboard-react"]||[]).push([[18],{258:function(t,a,e){"use strict";e.r(a);var n=e(0),r=e.n(n),l=e(32);a.default=function(){return r.a.createElement(r.a.Fragment,null,r.a.createElement(l.a,null,"Blank"))}},32:function(t,a,e){"use strict";var n=e(0),r=e.n(n);a.a=function(t){var a=t.children;return r.a.createElement("h1",{className:"my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"},a)}}}]);
+//# sourceMappingURL=18.35fc6cbf.chunk.js.map
